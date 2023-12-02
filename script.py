280f20e3a83746344f4f7c9724f25ac1193c8830
Leonardo Martella <leonardomartella2002@gmail.com>
2023-12-02 19:58:49 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,16 @@
+import requests
+
+def get_station_name(station_dict):
+    station_name = station_dict.get('designationOfficial', 'N/A')
+    return station_name
+
+def get_station_name_api():
+    query = requests.get("https://servicepoints.api.sbb.ch/v1/CH/STOP/ids=BNWD", headers={
+        X
+    })
+    print(query)
+    station_name = get_station_name(query.json())
+    return station_name
+
+print(get_station_name_api())
+
