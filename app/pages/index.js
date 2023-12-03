ddf98b4c9ad431a8407f7fd1e1161eacf5162a8f
akpekig <gillianakpeki@hotmail.co.uk>
2023-12-03 11:25:48 +0000

    Add Location input

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,14 +1,34 @@
 import '../styles/global.css';
+import '@geoapify/geocoder-autocomplete/styles/minimal.css';
+import { GeoapifyContext, GeoapifyGeocoderAutocomplete } from '@geoapify/react-geocoder-autocomplete';
 
 const Home = () => {
     return (
-        <div>
-        <header>
-        </header>
-        <main>
-            
-        </main>
-        </div>
+        <GeoapifyContext apiKey="889fd4a0d01540c0ad6a296c4571f0e9">
+            <header>
+            </header>
+            <main>
+            <GeoapifyGeocoderAutocomplete
+                placeholder="Enter address here"
+                value={value}
+                type={type}
+                lang={language}
+                position={position}
+                countryCodes={countryCodes}
+                limit={limit}
+                filterByCountryCode={filterByCountryCode}
+                filterByCircle={filterByCircle}
+                filterByRect={filterByRect}
+                filterByPlace={filterByPlace}
+                biasByCountryCode={biasByCountryCode}
+                biasByCircle={biasByCircle}
+                biasByRect={biasByRect}
+                biasByProximity={biasByProximity}
+                placeSelect={onPlaceSelect}
+                suggestionsChange={onSuggestionChange}
+            />
+            </main>
+        </GeoapifyContext>
     )
 }
 
